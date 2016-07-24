15c987b66f43149971a6bd13b61e91e055db8416
Shawn Li <xxiang13@gmail.com>
2016-07-24 17:17:47 -0500

    first commit

---
+++ database_sql/department_top_three_salaries.sql
@@ -0,0 +1,80 @@
+'''
+The Employee table holds all employees. Every employee has an Id, and there is also a column
+ for the department Id.
+
++----+-------+--------+--------------+
+| Id | Name  | Salary | DepartmentId |
++----+-------+--------+--------------+
+| 1  | Joe   | 70000  | 1            |
+| 2  | Henry | 80000  | 2            |
+| 3  | Sam   | 60000  | 2            |
+| 4  | Max   | 90000  | 1            |
+| 5  | Janet | 69000  | 1            |
+| 6  | Randy | 85000  | 1            |
++----+-------+--------+--------------+
+The Department table holds all departments of the company.
+
++----+----------+
+| Id | Name     |
++----+----------+
+| 1  | IT       |
+| 2  | Sales    |
++----+----------+
+Write a SQL query to find employees who earn the top three salaries in each of the department.
+For the above tables, your SQL query should return the following rows.
+
++------------+----------+--------+
+| Department | Employee | Salary |
++------------+----------+--------+
+| IT         | Max      | 90000  |
+| IT         | Randy    | 85000  |
+| IT         | Joe      | 70000  |
+| Sales      | Henry    | 80000  |
+| Sales      | Sam      | 60000  |
++------------+----------+--------+
+'''
+
+
+
+
+
+# given departmentId, get top 3 salary using correlated subquery
+
+select Salary, DepartmentId
+from Employee
+where DepartmentId = A.DepartmentId
+order by Salary desc limit 3
+
+# check if the salary if in top 3
+
+Salary in (select Salary
+			from Employee
+			where DepartmentId = E.DepartmentId
+			order by Salary desc limit 3)
+
+## final code
+# can't test on leetcode, saying mysql version doesn't support limit in subquery
+
+select D.Name as Department, E.Name as Employee, E.Salary as Salary
+from
+Employee E
+inner join Department D 
+on E.DepartmentId = D.Id
+where Salary in (select distinct Salary
+				 from Employee
+				 where DepartmentId = E.DepartmentId
+				 order by Salary desc limit 3)
+Order by Department, Salary desc
+
+
+## method 2
+# count number of Salary larger than the current one,
+# Where clause constains to the result with  the number less than 3
+
+select D.Name as Department, E.Name as Employee, E.Salary as Salary 
+from Employee E, Department D
+where (select COUNT(distinct Salary) from Employee 
+       where DepartmentId = E.DepartmentId and Salary > E.Salary) < 3
+and E.DepartmentId = D.Id 
+order by E.DepartmentId, E.Salary desc
+
---
+++ database_sql/nth_highest_salary.sql
@@ -0,0 +1,32 @@
+
+
+/*
+
+Write a SQL query to get the nth highest salary from the Employee table.
+
++----+--------+
+| Id | Salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+For example, given the above Employee table, the nth highest salary where n = 2 is 200.
+ If there is no nth highest salary, then the query should return null.
+*/
+
+# when lim take two argus, 1st one is where to start, 
+# 2nd is max number of elements return
+# so when max number of elements return larger than available elements.
+# it would return NULL
+# Here starting from  M = N-1 since the initial row is 0 (not 1)
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+DECLARE M INT;
+SET M=N-1;
+  RETURN (
+      # Write your MySQL query statement below.
+        SELECT DISTINCT Salary FROM Employee ORDER BY Salary 
+        DESC LIMIT M,1
+  );
+END
